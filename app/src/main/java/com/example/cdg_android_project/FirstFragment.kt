fca6b8861f68c73b9609a70ab3ac302810620a32
Ekaterina <60484474+e-uskova@users.noreply.github.com>
2024-06-29 14:27:00 +0300

    Task 2

---
+++ app/src/main/java/com/example/cdg_android_project/FirstFragment.kt
@@ -0,0 +1,30 @@
+package com.example.cdg_android_project
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cdg_android_project.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        binding.btnDetails.setOnClickListener() {
+            val fragment = SecondFragment()
+            val ft = getParentFragmentManager().beginTransaction()
+            ft.replace(R.id.frame_layout, fragment)
+            ft.commit()
+        }
+
+        (activity as AppCompatActivity).supportActionBar?.title = "Premier League Fixtures"
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        return binding.root
+    }
+}
