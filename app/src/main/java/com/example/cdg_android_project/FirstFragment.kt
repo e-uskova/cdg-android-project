56385960d23c4c071b7cc827b4c219fa90b7cc3c
Ekaterina <60484474+e-uskova@users.noreply.github.com>
2024-07-06 19:14:01 +0300

    Task 4

--- app/src/main/java/com/example/cdg_android_project/FirstFragment.kt
+++ app/src/main/java/com/example/cdg_android_project/FirstFragment.kt
@@ -1,26 +1,24 @@
 package com.example.cdg_android_project
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cdg_android_project.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+    private val previewAdapter: PreviewAdapter by lazy { PreviewAdapter() }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentFirstBinding.inflate(inflater, container, false)
-
-        binding.btnDetails.setOnClickListener() {
-            val fragment = SecondFragment()
-            val ft = getParentFragmentManager().beginTransaction()
-            ft.replace(R.id.frame_layout, fragment)
-            ft.commit()
-        }
 
         (activity as AppCompatActivity).supportActionBar?.title = "  Premier League Fixtures"
         (activity as AppCompatActivity).supportActionBar?.setDisplayUseLogoEnabled(true)
@@ -29,4 +27,35 @@ class FirstFragment : Fragment() {
 
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<RecyclerView>(R.id.recyclerView).apply {
+            this.adapter = previewAdapter
+            setHasFixedSize(true)
+            addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        }
+
+        // TODO get data
+        previewAdapter.submitList(generateFakeValues())
+
+        previewAdapter.setOnItemClickListener(object : PreviewAdapter.onItemClickListener {
+            override fun onItemClick(position: Int) {
+                val bundle = Bundle()
+                // TODO get id by position
+                bundle.putString("matchId", position.toString())
+
+                val details: SecondFragment = SecondFragment()
+                details.setArguments(bundle)
+
+                val ft = getParentFragmentManager().beginTransaction()
+                ft.replace(R.id.frame_layout, details)
+                ft.commit()
+            }
+        })
+    }
+
+    private fun generateFakeValues(): List<String> = List(20) { i -> "Match $i"}
 }
--- app/src/main/java/com/example/cdg_android_project/SecondFragment.kt
+++ app/src/main/java/com/example/cdg_android_project/SecondFragment.kt
@@ -1,12 +1,12 @@
 package com.example.cdg_android_project
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.cdg_android_project.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
@@ -19,6 +19,23 @@ class SecondFragment : Fragment() {
         (activity as AppCompatActivity).supportActionBar?.title = "Match details"
         (activity as AppCompatActivity).supportActionBar?.setDisplayUseLogoEnabled(false)
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        fun setMatchDetailsById(matchId: Int) {
+            // TODO get match info by id
+
+            // TODO set match info
+
+            val matchIdView = binding.root.findViewById<TextView>(R.id.txtId)
+            matchIdView.text = "Match ID: $matchId"
+        }
+
+        val bundle = arguments
+        if (bundle != null && bundle.containsKey("matchId")) {
+            val id = bundle.getString("matchId")?.toInt()
+            if (id != null) {
+                setMatchDetailsById(id)
+            }
+        }
 
         return binding.root
     }
